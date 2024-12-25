eab9e96f530e5d7e1c526cfbb20284c00690fd74
machida-haruaki <thinking18230rigin@gmail.com>
2024-12-25 09:04:35 +0900

    存在しないディレクトリ下のファイルを作成しようとした場合、ディレクトリを自動生成する

--- init.lua
+++ init.lua
@@ -12,6 +12,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 vim.cmd [[colorscheme gruvbox]]
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local dir = vim.fn.expand("<afile>:p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
+
 -- Leaderキーを Space に設定
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
